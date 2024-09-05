7c12078763c04b0dc4e3a2c74223319abd610579
Kemal Kazanc <Keemalkazanc@gmail.com>
2024-09-05 15:20:23 +0200

    Initial Commit

---
+++ Collection_Livre/Collection_LivreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Collection_LivreApp.swift
+//  Collection_Livre
+//
+//  Created by Kemal Kazanc on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Collection_LivreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
